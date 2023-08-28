a99c6190f53a6f31d8cb7a17bec4708ecd5b22cb
jmyou-muhayu <jmyou@muhayu.com>
2023-08-29 00:34:50 +0900

    fix: 일부 수정 사항 반영

--- server.ts
+++ server.ts
@@ -22,16 +22,23 @@ app.use(cors());
 
 app.post('/chat', async (req, res) => {
 	const { prompt } = req.body;
-	const completion = await openai.createCompletion({
-		model: 'text-davinci-003',
-		max_tokens: 1000,
-		temperature: 0,
-		prompt,
-	});
-	res.send(completion.data.choices[0].text);
+	try {
+		const completion = await openai.createCompletion({
+			model: 'text-davinci-003',
+			// model: 'gpt-4',
+			max_tokens: 1024,
+			temperature: 0,
+			prompt,
+		});
+		res.send(completion.data.choices[0].text);
+	} catch (err) {
+		//! FIXME  별도 예외 처리 필요
+		console.error(err);
+		res.send({ errorCode: 0, message: 'gpt api 에러' });
+	}
 });
 
-const PORT = 5000;
+const PORT = 4000;
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
--- src/api.ts
+++ src/api.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 export const getAnswer = ({ prompt }: { prompt: string }) => {
 	return axios
-		.post(import.meta.env.DEV ? 'http://localhost:5000/chat' : 'http://3.36.234.174/chat', {
+		.post(import.meta.env.DEV ? 'http://localhost:4000/chat' : 'http://3.36.234.174/chat', {
 			prompt,
 		})
 		.then((res) => {
 			return res.data;
 		})
 		.catch((e) => {
+			//! FIXME  별도 예외 처리 필요
 			console.log(e);
 		});
 };
